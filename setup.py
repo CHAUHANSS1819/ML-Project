ff7e78b932aef58123d6250c40a8868b72e8f154
Saurabh Singh <saurabhchauhan6388@gmail.com>
2023-07-28 08:38:24 +0530

    file updated

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirement(file_path:str)->list[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+     name="src",
+     version="0.0.1",
+     author="saurabh",
+     author_email="saurabhchauhan6388@gmail.com",
+     install_requirements=get_requirement("requirement.txt"),
+     packages=find_packages()
+)
